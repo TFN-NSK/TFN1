626e965e5a0c3347928800cb4626e8687efd35cf
TFN-NSK <tfn-novosibirsk@yandex.ru>
2024-03-25 18:25:37 +0700

    Первый коммит

---
+++ main1.js
@@ -0,0 +1,71 @@
+// console.log(2024)
+// console.log(20+'22');
+// console.log('Hello TFN Novosibirsk');
+// console.log('Good '+'day');
+// alert('Привет посетитель')
+var a = 10; // устаревший способ создания переменной var
+console.log(a);
+let b = 20 // современный способ создания изменяемой переменной let
+console.log(b);
+const c = 30// современный способ создания не изменяемой переменной const
+console.log(c);
+a = 500
+console.log(a);
+b = 9000
+console.log(b);
+
+// Числовой
+// infinity математическая бесконечность больше любого числа
+console.log(Infinity);
+
+// NaN вычислительная ошибка
+console.log(NaN);
+console.log('stroka'/20);
+console.log('stroka '+20);
+// исключение 
+console.log((NaN **0));
+
+// Bigint просто добавляем в конце букву n и снимаем ограничения для числа
+console.log(15465486745454645456n);
+
+// Строковый
+
+let str1 = 'jojo'
+let str2 = "jojo 2"
+let str3 = `Anime ${2 +2 * 100} ${str1}`
+
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
+// bulean логический
+
+let check = true
+let offcheck = false
+console.log(check);
+console.log(offcheck);
+console.log(4 > 1);
+console.log(4 < 1);
+
+// NULL
+console.log(null);
+
+// typeof
+
+console.log(typeof undefined);
+console.log(typeof 2);
+console.log(typeof 10n);
+console.log(typeof 'cat');
+console.log(typeof true);
+
+
+document.getElementById('first').innerHTML = 'Hello TFN'
+console.log(1);
+
+
+
+
+
+
+
+
