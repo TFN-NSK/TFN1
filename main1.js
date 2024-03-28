21c2d92270556ccbb6cf99b55396c104918371d0
TFN-NSK <tfn-novosibirsk@yandex.ru>
2024-03-28 14:01:14 +0700

    Коммит к третьему уроку

--- main1.js
+++ main1.js
@@ -3,71 +3,121 @@
 // console.log('Hello TFN Novosibirsk');
 // console.log('Good '+'day');
 // alert('Привет посетитель')
-var a = 10; // устаревший способ создания переменной var
-console.log(a);
-let b = 20 // современный способ создания изменяемой переменной let
-console.log(b);
-const c = 30// современный способ создания не изменяемой переменной const
-console.log(c);
-a = 500
-console.log(a);
-b = 9000
-console.log(b);
+// var a = 10; // устаревший способ создания переменной var
+// console.log(a);
+// let b = 20 // современный способ создания изменяемой переменной let
+// console.log(b);
+// const c = 30// современный способ создания не изменяемой переменной const
+// console.log(c);
+// a = 500
+// console.log(a);
+// b = 9000
+// console.log(b);
 
 // Числовой
 // infinity математическая бесконечность больше любого числа
-console.log(Infinity);
+// console.log(Infinity);
 
 // NaN вычислительная ошибка
-console.log(NaN);
-console.log('stroka'/20);
-console.log('stroka '+20);
+// console.log(NaN);
+// console.log('stroka'/20);
+// console.log('stroka '+20);
 // исключение 
-console.log((NaN **0));
+// console.log((NaN **0));
 
 // Bigint просто добавляем в конце букву n и снимаем ограничения для числа
-console.log(15465486745454645456n);
+// console.log(15465486745454645456n);
 
 // Строковый
 
-let str1 = 'jojo'
-let str2 = "jojo 2"
-let str3 = `Anime ${2 +2 * 100} ${str1}`
+// let str1 = 'jojo'
+// let str2 = "jojo 2"
+// let str3 = `Anime ${2 +2 * 100} ${str1}`
 
-console.log(str1);
-console.log(str2);
-console.log(str3);
+// console.log(str1);
+// console.log(str2);
+// console.log(str3);
 
 // bulean логический
 
-let check = true
-let offcheck = false
-console.log(check);
-console.log(offcheck);
-console.log(4 > 1);
-console.log(4 < 1);
+// let check = true
+// let offcheck = false
+// console.log(check);
+// console.log(offcheck);
+// console.log(4 > 1);
+// console.log(4 < 1);
 
 // NULL
-console.log(null);
+// console.log(null);
 
 // typeof
 
-console.log(typeof undefined);
-console.log(typeof 2);
-console.log(typeof 10n);
-console.log(typeof 'cat');
-console.log(typeof true);
+// console.log(typeof undefined);
+// console.log(typeof 2);
+// console.log(typeof 10n);
+// console.log(typeof 'cat');
+// console.log(typeof true);
 
 
-document.getElementById('first').innerHTML = 'Hello TFN'
-console.log(1);
-console.log(2);
-console.log(3);
+// document.getElementById('first').innerHTML = 'Hello TFN'
+// console.log(1);
+// console.log(2);
+// console.log(3);
 
-console.log(12 + ' декабрь');
-console.log('Добро'+ ' пожаловать'+' на'+' курс'+' Евгения');
+// console.log(12 + ' декабрь');
+// console.log('Добро'+ ' пожаловать'+' на'+' курс'+' Евгения');
 // alert('20.03.2024')
-document.getElementById('first').innerHTML = 'Hello World'
+// document.getElementById('first').innerHTML = 'Hello World'
+// document.getElementById('first').innerHTML = 666
+// document.getElementById('first').innerHTML = 'anime'
+
+// let a = 'Evgenia'
+// a = 123
+// console.log(a);
+
+// document.querySelector('#one').innerHTML = 666
+// document.querySelector('p').innerHTML = 'Складская техника'
+
+// let inp = document.querySelector('.text')
+// let btn = document.querySelector('.btn')
+// let div = document.querySelector('.qwe')
+
+
+
+// btn.onclick = function(){
+//     console.log('da');
+//     let e = inp.value
+//     div.innerHTML = e
+//     inp.value = ''
+// }
+
+let a = prompt('Сколько у тебя денег?')
+// console.log(a);
+
+if(a>100){
+    alert('yes')
+}
+else{
+    console.log('no')
+}
+
+
+  
+    
+
+    
+
+
+
+
+
+    
+
+
+
+
+
+
 
 
 
