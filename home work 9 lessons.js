13a548bf87c12df04021bbf93e8423e953c3aa2d
TFN-NSK <tfn-novosibirsk@yandex.ru>
2024-04-16 11:05:18 +0700

    ДЗ к 9 уроку

---
+++ home work 9 lessons.js
@@ -0,0 +1,31 @@
+
+// Задача 1
+
+// let button1 = document.querySelector('.button1')
+// let resalt = [15,12,1984,'Isaeva','Evgenia',11,24,25,17,true]
+// button1.onclick = function(){
+//     resalt.innerHTML = (resalt[5])
+//     console.log(resalt[5]);
+// }
+// Задача 2
+
+// let btn5 = document.querySelector('.btn5')
+// let resalt = [1,2,3,4,5,6,7,8,9,10]
+// // console.log(resalt);
+
+// btn5.onclick = function(){
+//     resalt.splice(1,1)
+//     console.log(resalt);
+// }
+
+//Задача 4
+
+// let X = [
+//         [1,2,3,],
+//         [4,5,6],
+//         [7,8,9]
+//     ]
+//     btn5.onclick = function(){
+//         resalt = X.reverse()
+//         console.log(resalt);
+//         }
--- main1.js
+++ main1.js
@@ -743,15 +743,47 @@
 // // console.log(b);
 // // // console.log(b[0][0]);
 
-// // for(let i = 0; i<b.length; i++){
-// //     // console.log(b[i]);
-// //     let c = b[i] // c - массив [1,2,3]
-// //     for(let k = 0; c.length> k;k++){
-// //         console.log(c[k]);
-// //     }
+// for(let i = 0; i<b.length; i++){
+//     // console.log(b[i]);
+//     let c = b[i] // c - массив [1,2,3]
+//     for(let k = 0; c.length> k;k++){
+//         console.log(c[k]);
+//     }
 // // }
 // for(let i = 0; i<b.length; i++){
 //         for(let j = 0; b[i].length> j;j++){
 //             console.log(b[i][j]);
 //         }
 //     }
+//Домашнее задание к 9-му уроку.
+
+// Задача 1
+
+// let button1 = document.querySelector('.button1')
+// let resalt = [15,12,1984,'Isaeva','Evgenia',11,24,25,17,true]
+// button1.onclick = function(){
+//     resalt.innerHTML = (resalt[5])
+//     console.log(resalt[5]);
+// }
+// Задача 2
+
+// let btn5 = document.querySelector('.btn5')
+// let resalt = [1,2,3,4,5,6,7,8,9,10]
+// // console.log(resalt);
+
+// btn5.onclick = function(){
+//     resalt.splice(1,1)
+//     console.log(resalt);
+// }
+
+//Задача 4
+
+// let X = [
+//         [1,2,3,],
+//         [4,5,6],
+//         [7,8,9]
+//     ]
+//     btn5.onclick = function(){
+//         resalt = X.reverse()
+//         console.log(resalt);
+//         }
